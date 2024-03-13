9755ca6f1bd43564b70c7f40c28bb6ff42459e26
Dinesh Magar <magard1010@gmail.com>
2024-03-13 20:06:16 +0545

    project application setup

--- ECOMM/settings.py
+++ ECOMM/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "ecomm_app",
+    "app",
 ]
 
 MIDDLEWARE = [
@@ -63,6 +63,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                
             ],
         },
     },
