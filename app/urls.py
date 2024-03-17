9a14e0a5c8043ad69578134711863bc8fde26bc9
Dinesh Magar <magard1010@gmail.com>
2024-03-18 02:39:31 +0545

    customer registration messages form validation

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
     path("mobile/<slug:data>", views.mobile, name="mobiledata"),
     
     path("login/", views.login, name="login"),
-    path("registration/", views.customerregistration, name="customerregistration"),
+    path("registration/", views.CustomerRegistrationView.as_view(), name="customerregistration"),
     path("checkout/", views.checkout, name="checkout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
