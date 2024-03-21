5688ed3df70bf937b98bf8905d21493e9e5f406f
Dinesh Magar <magard1010@gmail.com>
2024-03-21 23:56:48 +0545

    Login logout change password

--- app/urls.py
+++ app/urls.py
@@ -2,22 +2,59 @@ from django.urls import path
 from app import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm, PasswordChangeForm
 
 urlpatterns = [
     # path("", views.home),
-    path('', views.HomeView.as_view(), name='home'),
-    
-    path("product-detail/<int:pk>", views.ProductDetailView.as_view(), name="product-detail"),
+    path("", views.HomeView.as_view(), name="home"),
+    path(
+        "product-detail/<int:pk>",
+        views.ProductDetailView.as_view(),
+        name="product-detail",
+    ),
     path("cart/", views.add_to_cart, name="add-to-cart"),
     path("buy/", views.buy_now, name="buy-now"),
     path("profile/", views.profile, name="profile"),
     path("address/", views.address, name="address"),
     path("orders/", views.orders, name="orders"),
-    path("changepassword/", views.change_password, name="changepassword"),
+    
     path("mobile/", views.mobile, name="mobile"),
     path("mobile/<slug:data>", views.mobile, name="mobiledata"),
+    path(
+        "accounts/login/",
+        auth_views.LoginView.as_view(
+            template_name="app/login.html", authentication_form=LoginForm
+        ),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+    path(
+        "passwordchange/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="app/passwordchange.html",
+            form_class=MyPasswordChangeForm,
+            next_page="login",
+        ),
+        name="passwordchange",
+    ),
+    path(
+        "passwordchangedone/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="app/passwordchangedone.html",
+            form_class=MyPasswordChangeForm,
+            next_page="login",
+        ),
+        name="passwordchangedone",
+    ),
     
-    path("login/", views.login, name="login"),
-    path("registration/", views.CustomerRegistrationView.as_view(), name="customerregistration"),
+    
+    path(
+        "registration/",
+        views.CustomerRegistrationView.as_view(),
+        name="customerregistration",
+    ),
+    
+    
     path("checkout/", views.checkout, name="checkout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -52,8 +52,7 @@ def orders(request):
     return render(request, "app/orders.html")
 
 
-def change_password(request):
-    return render(request, "app/changepassword.html")
+
 
 
 def mobile(request, data=None):
